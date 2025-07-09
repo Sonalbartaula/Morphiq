7d328f33f70fc1c843f45012ca272fb3e372afb8
Sonalbartaula <bartaulasonal.nd@gmail.com>
2025-07-09 18:48:34 +0545

    transformation

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -2,9 +2,8 @@ import Header from "@/components/shared/Header";
 import Transformationform from "@/components/shared/Transformationform";
 import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
-import { RedirectToSignIn } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/dist/server/api-utils";
+
 
 const AddTransformationTypePage = async ({ params : {type} }:SearchParamProps) => {
     const {userId,redirectToSignIn} = await auth();
@@ -17,12 +16,14 @@ const AddTransformationTypePage = async ({ params : {type} }:SearchParamProps) =
             title={transformation.title}
             subtitle={transformation.subTitle}
             />
+        <section className="mt-10">
             <Transformationform
                 action="Add"
                 userId={user._id}
                 type = {transformation.type as TransformationTypeKey}
                 creditBalance={user.creditBalance}
                 />
+        </section>       
         </>    
     );
 }
